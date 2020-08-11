56771e82ae891f6af5d914e222768a9d9ed11550
remitsan <51080254+remitsan@users.noreply.github.com>
2020-08-11 10:14:53 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            bool repeat = true;
+            while (repeat)
+            {
+                do
+                {
+                    Console.Write("A = ");
+                }
+                while (!int.TryParse(Console.ReadLine(), out a));
+                do
+                {
+                    Console.Write("B = ");
+                }
+                while (!int.TryParse(Console.ReadLine(), out b));
+                if (a <= 0 || b <= 0)
+                {
+                    Console.WriteLine("Ошибка, попробовать еще раз? Y/N");
+                    var ki = Console.ReadKey(true);
+                    if (ki.Key != ConsoleKey.Y)
+                        repeat = false;
+                    continue;
+                }
+                Console.WriteLine("Д: {0}, Ш: {1}, Площадь: {2}", a, b, a * b);
+                repeat = false;
+                Console.ReadKey();
+            }
+        }
+    }
+}
